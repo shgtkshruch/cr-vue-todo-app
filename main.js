a8babc9282cee2fc32951b5d3b0fb88b6fd4a071
shgtkshruch <shgtk.shruch@gmail.com>
2018-09-23 23:52:16 +0900

    Remove todo and change state

--- main.js
+++ main.js
@@ -23,6 +23,13 @@ const app = new Vue({
 
       comment.value = '';
     },
+    changeState(todo) {
+      todo.state = todo.state ? 0 : 1;
+    },
+    remove(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
   },
   watch: {
     todos: {
