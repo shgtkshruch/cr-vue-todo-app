3a9682750cac997648d4847dc62ff40c3e15a434
shgtkshruch <shgtk.shruch@gmail.com>
2018-09-23 23:33:03 +0900

    Add local storage script

---
+++ local-storage.js
@@ -0,0 +1,18 @@
+// https://jp.vuejs.org/v2/examples/todomvc.html
+const STORAGE_KEY = 'todos-vuejs-demo'
+
+export default {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+};
---
+++ main.js
@@ -0,0 +1,12 @@
+import localStorage from './local-storage.js';
+
+console.log(localStorage);
+const app = new Vue({
+  el: '#app',
+  data: {
+
+  },
+  methods: {
+
+  },
+});
